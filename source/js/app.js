a987c9bfd2f7d7b9fe595e3faa4bee0b249cf756
andrewFountain <founts24@gmail.com>
2016-06-14 09:04:20 +1000

    initial commit

---
+++ source/js/app.js
@@ -0,0 +1,8 @@
+const webpack = require('./test.js');
+
+const ramda  = require('ramda');
+const Maybe  = require('data.maybe');
+const IO     = require('data.io');
+const Either = require('data.either');
+
+const { map, prop } = ramda;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  context: __dirname,
+  entry: './source/js/app.js',
+  output: {
+    path: './dist',
+    filename: 'app.bundle.js',
+    publicPath: '/dist/'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+          cacheDirectory: '.webpackcache'
+        }
+      },
+    ]
+  }
+};
